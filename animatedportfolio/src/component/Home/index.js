9dd796c1818dfcddcc6b9e174d7cff21219ad106
Ian Elliott <ie2173@columbia.edu>
2022-04-12 20:13:49 -0700

    begin contact page

--- animatedportfolio/src/component/Home/index.js
+++ animatedportfolio/src/component/Home/index.js
@@ -4,6 +4,7 @@ import logoS from '../../assets/images/logo-s.png'
 import { Link } from 'react-router-dom'
 import AnimatedLetters from '../AnimatedLetters'
 import Logo from './components/Logo'
+import Loader from 'react-loaders'
 
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
@@ -31,34 +32,37 @@ const Home = () => {
   }, [])
 
   return (
-    <div className="container home-page">
-      <div className="text-zone">
-        <h1>
-          <span className={letterClass}>H</span>
-          <span className={`${letterClass} _12`}>i</span>
-          <br />
-          <span className={`${letterClass} _13`}>I'</span>
-          <span className={`${letterClass} _14`}>m</span>
-          <img src={logoS} alt="developer" />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={nameArray}
-            idx={15}
-          />
-          <br />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={jobArray}
-            idx={22}
-          />
-        </h1>
-        <h2>Frontend Developer & Web3 Developer</h2>
-        <Link to="/contact" className="flat-button">
-          CONTACT
-        </Link>
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i</span>
+            <br />
+            <span className={`${letterClass} _13`}>I'</span>
+            <span className={`${letterClass} _14`}>m</span>
+            <img src={logoS} alt="developer" />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={22}
+            />
+          </h1>
+          <h2>Frontend Developer & Web3 Developer</h2>
+          <Link to="/contact" className="flat-button">
+            CONTACT
+          </Link>
+        </div>
+        <Logo />
       </div>
-      <Logo />
-    </div>
+      <Loader type="pacman" />
+    </>
   )
 }
 
